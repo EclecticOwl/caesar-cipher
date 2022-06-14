d182cd337667185de9a677ec9eae9679e701bada
Michael Tillman <mnamllit@gmail.com>
2022-06-13 21:31:23 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+def cipher_text():
+
+    text_input = input('Gimme a sentence to cipher: \n\n>>> ')
+    shift_amount = input('Number please: \n\n>>> ')
+
+    cipher = []
+    for i in text_input:
+        number_code = ord(i) + int(shift_amount)
+        letter_code = chr(number_code)
+        cipher.append(letter_code)
+    
+    cipher = ' '.join(cipher)
+    cipher = cipher.replace(' ', '')
+
+    print(f'Plain text is: {text_input}')
+    print(f'Ciphered text is: {cipher}')
+    print(f'Ciphered using ROT{shift_amount}')
+
+def decipher_text():
+
+    text_input = input('Gimme a sentence to decipher: \n\n>>> ')
+    shift_amount = input('Number please: \n\n>>> ')
+
+    plain = []
+    for i in text_input:
+        number_code = ord(i) - int(shift_amount)
+        letter_code = chr(number_code)
+        plain.append(letter_code)
+    
+    plain = ' '.join(plain)
+    plain = plain.replace(' ', '')
+
+    print(f'Plain text is: {plain}')
+
+
+
+
+
