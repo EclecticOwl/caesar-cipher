651a3c6295648d3c4470f54b865548ad1745ba9f
Michael Tillman <mnamllit@gmail.com>
2022-06-13 22:56:49 -0500

    Added copy module for convenience.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pyperclip as pc
+
 def cipher_text():
     text_input = input('Gimme a sentence to cipher: \n\n>>> ')
     shift_amount = input('Number please: \n\n>>> ')
@@ -14,8 +16,13 @@ def cipher_text():
     print(f'Ciphered text is: {cipher}')
     print(f'Ciphered using ROT{shift_amount}')
 
-def decipher_text():
 
+    confirmation = input("Would you like to copy to clipboard?\n Please enter yes or y")
+    if confirmation == 'yes' or 'y':
+        pc.copy(cipher)
+        print('Text copied to clipboard.')
+
+def decipher_text():
     text_input = input('Gimme a sentence to decipher: \n\n>>> ')
     shift_amount = input('Number please: \n\n>>> ')
 
@@ -26,22 +33,24 @@ def decipher_text():
         plain.append(letter_code)
     
     plain = ''.join(plain)
-
     print(f'Plain text is: {plain}')
 
 
 def menu():
-    print('1: Cipher text')
+    print('\n1: Cipher text')
     print('2: Decipher text')
-    choice = input('\nPlease choose a number based off of the options provided above: \n\n')
+    print('3: Exit program')
+    choice = input('\nPlease choose a number based off of the options provided above:\n\n>>> ')
 
     if choice == '1':
         cipher_text()
     elif choice == '2':
         decipher_text()
+    elif choice == '3':
+        print('\nExiting program.')
+        return
     else:
-        print('That is not a valid choice. \n\nNow exiting function....')
-
-
+        print('\nNot an option. Let us try this again.')
+        menu()
 
 menu()
